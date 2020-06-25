ff3fd3c73e8ec2186d11a5c92c915765db01b9e4
Yaroslav <elektroyar@yandex.ru>
2020-06-25 14:48:38 +0300

    добавлена лиценция

--- include/easy-email.hpp
+++ include/easy-email.hpp
@@ -1,5 +1,28 @@
-#ifndef EMAILAPI_HPP_INCLUDED
-#define EMAILAPI_HPP_INCLUDED
+/*
+* easy-email-cpp - C ++ library for sending emails
+*
+* Copyright (c) 2019 Elektro Yar. Email: git.electroyar@gmail.com
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#ifndef EASY_EMAIL_HPP_INCLUDED
+#define EASY_EMAIL_HPP_INCLUDED
 
 #include <string>
 #include "curl/curl.h"
@@ -245,4 +268,4 @@ public:
     }
 };
 
-#endif // EMAILAPI_HPP_INCLUDED
+#endif // EASY_EMAIL_HPP_INCLUDED
