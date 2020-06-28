53e4a017f89fdb6850748812a1b089659db13c44
Yaroslav <elektroyar@yandex.ru>
2020-06-28 04:05:49 +0300

    добавлена возможность отправлять текстовые файлы

--- code-blocks/example/main.cpp
+++ code-blocks/example/main.cpp
@@ -11,5 +11,13 @@ int main() {
     std::this_thread::sleep_for(std::chrono::milliseconds(10000));
     easy_email.send("to_two_example@yandex.ru", u8"Робот", u8"Превед, человег! Ещо раз!");
     easy_email.send(u8"Робот", u8"Превед самому себе!");
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    easy_email.send(email, u8"Файл самому себе!", u8"Вот файлы",
+        {"Пример.txt", "Пример2.txt"},
+        {u8"Вот мы и добрались до самого сокровеного...", u8"Вот мы и добрались до самого сокровеного... Наверно..."});
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    easy_email.send(u8"Файл самому себе 2.0", u8"Вот файлы еще раз",
+        {"Пример3.txt", "Пример4.txt"},
+        {u8"Вот мы и добрались до самого сокровеного... Или нет...", u8"Вот мы и добрались до самого сокровеного... Наверно... Хотя.."});
     return 0;
 }
--- include/easy-email.hpp
+++ include/easy-email.hpp
@@ -25,7 +25,9 @@
 #define EASY_EMAIL_HPP_INCLUDED
 
 #include <string>
+#include <vector>
 #include "curl/curl.h"
+#include "base64.h"
 
 class EasyEmail {
 private:
@@ -33,6 +35,7 @@ private:
     std::string email;          /**< Электронная почта */
     std::string password;       /**< Пароль от почты */
     std::string sert_file;      /**< Файл сертификата */
+    const uint32_t port = 465;
 
     bool is_skip_hostname_verification = false;
     bool is_skip_peer_verification = false;
@@ -145,18 +148,29 @@ public:
     }
 
     /** \brief отправить сообщение самому себе
-     * \param theme тема сообщения
-     * \param msg сообщение
+     * \param theme Тема сообщения
+     * \param msg Сообщение
      * \return CURLE_OK в случае успеха
      */
     int send(const std::string &theme, const std::string &msg) {
         return send(email, theme, msg);
     }
 
+    /** \brief Отправить текстовый файл на email
+     * \param theme Тема сообщения
+     * \param msg Сообщение
+     * \param file_name Массив имен файлов
+     * \param file_content Тело файлов
+     * \return CURLE_OK в случае успеха
+     */
+    int send(const std::string &theme, const std::string &msg, const std::vector<std::string> &file_name, const std::vector<std::string> &file_content) {
+        return send(email, theme, msg, file_name, file_content);
+    }
+
     /** \brief Отправить сообщение на email
-     * \param toemail кому отправить сообщение
-     * \param theme тема сообщения
-     * \param msg сообщение
+     * \param toemail Кому отправить сообщение
+     * \param theme Тема сообщения
+     * \param msg Сообщение
      * \return CURLE_OK в случае успеха
      */
     int send(const std::string &toemail, const std::string &theme, const std::string &msg) {
@@ -183,7 +197,152 @@ public:
              * для запроса соединения на основе SSL.
              */
             curl_easy_setopt(curl, CURLOPT_URL, url_mailserver.c_str());
-            curl_easy_setopt(curl, CURLOPT_PORT, 465);
+            curl_easy_setopt(curl, CURLOPT_PORT, port);
+            curl_easy_setopt(curl, CURLOPT_CAINFO, sert_file.c_str());
+
+            /* Если вы хотите подключиться к сайту, который не использует сертификат,
+             * подписанный одним из сертификатов в вашем комплекте CA,
+             * вы можете пропустить проверку сертификата сервера.
+             * Это делает соединение ОЧЕНЬ МЕНЬШЕ БЕЗОПАСНЫМ.
+             *
+             * Если у вас есть сертификат CA для сервера, хранящийся где-то еще,
+             * чем в комплекте по умолчанию,
+             * тогда вам может пригодиться параметр CURLOPT_CAPATH.
+             */
+            if(is_skip_peer_verification) curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+
+            /* Если сайт, к которому вы подключаетесь, использует другое имя хоста,
+             * отличное от того, что они указали в полях commonName (или subjectAltName)
+             * сертификата сервера, libcurl откажется подключиться.
+             * Вы можете пропустить эту проверку,
+             * но это сделает соединение менее безопасным. */
+            if(is_skip_hostname_verification) curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+            /* Обратите внимание, что эта опция строго не обязательна,
+             * ее пропуск приведет к тому, что libcurl отправит команду MAIL FROM
+             * с пустыми данными отправителя.
+             * Все автоответчики должны иметь пустой обратный путь и должны быть
+             * направлены на адрес в обратном пути, который их инициировал.
+             * В противном случае они могут вызвать бесконечный цикл.
+             * См. RFC 5321 Раздел 4.5.5 для более подробной информации.
+             */
+            std::string& from = email;
+            curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from.c_str());
+
+            /* Добавьте двух получателей,
+             * в данном конкретном случае они соответствуют
+             * Кому: и Cc: адресаты в заголовке,
+             * но они могут быть любыми получателями.
+             */
+            recipients = curl_slist_append(recipients, toemail.c_str());
+            curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+
+            /* Мы используем функцию обратного вызова,
+             * чтобы указать полезную нагрузку (заголовки и тело сообщения).
+             * Вы можете просто использовать опцию CURLOPT_READDATA,
+             * чтобы указать указатель FILE для чтения.
+             */
+            curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
+            curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
+            curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+
+            /* Поскольку трафик будет зашифрован,
+             * очень полезно включить отладочную информацию в libcurl,
+             *  чтобы увидеть, что происходит во время передачи
+             */
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+
+            curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error_buffer);
+            curl_easy_setopt(curl, CURLOPT_TIMEOUT, TIME_OUT); // выход через TIME_OUT сек
+
+            /* Отправить сообщение */
+            res = curl_easy_perform(curl);
+
+            /* Проверьте на ошибки */
+            if(res != CURLE_OK) {
+                //fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                //      curl_easy_strerror(res));
+                std::cerr << "EasyEmail"<< std::endl;
+                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+                std::cerr << "Libcurl error in send(), code:\n";
+                std::cerr << "Error: " << error_buffer;
+                std::cerr << std::endl << res << std::endl;
+
+                /* Освободить список получателей */
+                curl_slist_free_all(recipients);
+                curl_easy_cleanup(curl);
+                return res;
+            }
+
+            /* Освободить список получателей */
+            curl_slist_free_all(recipients);
+            curl_easy_cleanup(curl);
+        }
+        return (int)res;
+    }
+
+    /** \brief Отправить текстовый файл на email
+     * \param toemail Кому отправить сообщение
+     * \param theme Тема сообщения
+     * \param msg Сообщение
+     * \param file_name Массив имен файлов
+     * \param file_content Тело файлов
+     * \return CURLE_OK в случае успеха
+     */
+    int send(const std::string &toemail, const std::string &theme, const std::string &msg, const std::vector<std::string> &file_name, const std::vector<std::string> &file_content) {
+        struct curl_slist *recipients = NULL;
+        struct upload_status upload_ctx;
+
+        upload_ctx.lines_read = 0;
+        upload_ctx.msg = "To: <" + toemail + ">\r\n";
+        upload_ctx.msg += "From: <" + email + ">\r\n";
+        upload_ctx.msg += "Subject: " + theme + "\r\n";
+        upload_ctx.msg += "Content-Type: multipart/mixed; boundary=MixedEasyEmailBoundary\r\n";
+        upload_ctx.msg += "\r\n"; /* пустая строка для разделения заголовков от тела, см. RFC5322 */
+        upload_ctx.msg += "--MixedEasyEmailBoundary\r\n";
+        upload_ctx.msg += "Content-Type: multipart/alternative; boundary=AlternativeEasyEmailBoundary\r\n";
+        upload_ctx.msg += "\r\n";
+        upload_ctx.msg += "--AlternativeEasyEmailBoundary\r\n";
+        upload_ctx.msg += "Content-Type: text/plain; charset=\"utf-8\"\r\n";
+        upload_ctx.msg += "\r\n";
+        upload_ctx.msg += msg + "\r\n";
+        upload_ctx.msg += "\r\n";
+        upload_ctx.msg += "--AlternativeEasyEmailBoundary\r\n";
+        upload_ctx.msg += "Content-Type: multipart/related; boundary=RelatedEasyEmailBoundary\r\n";
+        upload_ctx.msg += "\r\n";
+        for(size_t i = 0; i < file_name.size(); ++i) {
+            std::string file_content_encoded;
+            if(!Base64::Encode(file_content[i], &file_content_encoded)) {
+                std::cout << "Failed to encode input string" << std::endl;
+                return -1;
+            }
+            upload_ctx.msg += "--RelatedEasyEmailBoundary\r\n";
+            upload_ctx.msg += "Content-Type: text/plain; name=\"" + file_name[i] + "\"\r\n";
+            upload_ctx.msg += "Content-Transfer-Encoding: base64\r\n";
+            upload_ctx.msg += "Content-Disposition: attachment; filename=\"" + file_name[i] + "\"\r\n";
+            upload_ctx.msg += "\r\n"; /* пустая строка для разделения заголовков от тела, см. RFC5322 */
+            upload_ctx.msg += file_content_encoded + "\r\n";
+            upload_ctx.msg += "\r\n"; /* пустая строка для разделения заголовков от тела, см. RFC5322 */
+        }
+        upload_ctx.msg += "--RelatedEasyEmailBoundary--\r\n";
+        upload_ctx.msg += "\r\n";
+        upload_ctx.msg += "--AlternativeEasyEmailBoundary--\r\n";
+        upload_ctx.msg += "\r\n";
+        upload_ctx.msg += "--MixedEasyEmailBoundary--\r\n\0";
+
+        CURLcode res = CURLE_OK;
+        CURL *curl = curl_easy_init();
+        if(curl) {
+            /* Установите имя пользователя и пароль */
+            curl_easy_setopt(curl, CURLOPT_USERNAME, email.c_str());
+            curl_easy_setopt(curl, CURLOPT_PASSWORD, password.c_str());
+
+            /* Это URL вашего почтового сервера. Обратите внимание
+             * на использование smtps: // вместо smtp: //
+             * для запроса соединения на основе SSL.
+             */
+            curl_easy_setopt(curl, CURLOPT_URL, url_mailserver.c_str());
+            curl_easy_setopt(curl, CURLOPT_PORT, port);
             curl_easy_setopt(curl, CURLOPT_CAINFO, sert_file.c_str());
 
             /* Если вы хотите подключиться к сайту, который не использует сертификат,
